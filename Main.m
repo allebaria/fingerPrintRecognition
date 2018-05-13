572ddf72b2485788f9a4da9526c28658d9b0beb6
Guille Casado <g-tech@parcbarcelona.com>
2018-05-13 20:18:12 +0200

    minor changes

--- Main.m
+++ Main.m
@@ -5,6 +5,7 @@ figure(1)
 imshow(im)
 title('Original Image 1')
 
+
 %Preprocessing
 [Ithin, MinutaeMatrixComplex] = ext_finger(im,1);
 figure(2)
@@ -12,14 +13,20 @@ imshow(Ithin)
 title('Thinned Image 1')
 
 
-
 %Processing
+%Get Minutiaes from original image
 [Bifurcations,Terminations,BifCentr,TermCentr]=getMinutaes(Ithin);
+
+
+%Getting and Preprocessing second image
 im2=imread('./DB1_B/105_8.tif');
 [Ithin2,MinutaeMatrixComplex2] = ext_finger(im2,1);
 figure(3)
 imshow(Ithin2)
 title('Thinned Image 2')
+
+
+%Translating second image to match with the original one
 center=findCenter(MinutaeMatrixComplex);
 center2=findCenter(MinutaeMatrixComplex2);
 Ithin3=imageTranslation(center,center2,Ithin2);
